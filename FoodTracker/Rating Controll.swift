9a74a5a84eabd439179ebe40973d37589c76176f
Kaden Storrs <kadenstorrs@Kadens-MacBook-Pro.local>
2019-08-30 10:02:53 -0600

    Added a view controller and a do something func in Viewcontroller

---
+++ FoodTracker/Rating Controll.swift
@@ -0,0 +1,19 @@
+//
+//  Rating Controll.swift
+//  FoodTrackerTests
+//
+//  Created by Kaden Storrs on 8/30/19.
+//  Copyright © 2019 Kaden Storrs. All rights reserved.
+//
+
+import UIKit
+
+class Rating_Controll: UIView {
+
+    //MARK: Initialization
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+      
+    }
+}
